5a13b9ab483e30cd5acae267368fcb3a459feb45
Rahul <srahul3@github.com>
2023-03-13 12:52:48 +0530

    Update trigger_n_actions.sh

--- scripts/trigger_n_actions.sh
+++ scripts/trigger_n_actions.sh
@@ -6,5 +6,5 @@ echo "arg 3: $3"
 count=$1
 for i in $(seq $count); do
     echo "Triggering the workflow# $i"
-    curl -L -X POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer $2" $3 -d '{"event_type":"aop-test-trigger","client_payload":{"unit":false,"integration":true}}'
+    curl -i -L -X POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer $2" $3 -d '{"event_type":"aop-test-trigger","client_payload":{"unit":false,"integration":true}}'
 done
