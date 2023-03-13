9024cf46c39227ef574089fd54564acddfa172d9
Rahul <srahul3@github.com>
2023-03-13 12:38:02 +0530

    Update trigger_n_actions.sh

--- scripts/trigger_n_actions.sh
+++ scripts/trigger_n_actions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {1..$1}
-do
+count=$1
+for i in $(seq $count); do
     echo "Triggering the workflow# $i"
     curl -X POST -H "Content-Type: application/vnd.github+json" -H "Authorization: Bearer $2" -d '{"event_type":"aop-test-trigger","client_payload":{}}' $3
 done
